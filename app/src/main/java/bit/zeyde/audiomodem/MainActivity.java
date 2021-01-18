f755948d4c3173b60af5cf161b0906532e220d73
chuck <fallencx@gmail.com>
2021-01-18 16:08:15 -0700

    Update to compile on API 30

--- app/src/main/java/bit/zeyde/audiomodem/MainActivity.java
+++ app/src/main/java/bit/zeyde/audiomodem/MainActivity.java
@@ -5,8 +5,8 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,7 +16,7 @@ import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
     private EditText editText = null;
